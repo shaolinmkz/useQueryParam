be04c9ed4fbab9493f75bdcce8069af2624607bb
shaolinmkz007 <emeka@softcom.ng>
2020-05-03 17:18:56 +0100

    run build and publish on npm

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @function useQueryParam
+ * @description extracts all query params from the URL
+ * @param {string} queries - query string
+ * @returns {object} - returns an object of query param properties
+ */
+declare const useQueryParam: (queries?: string) => any;
+//# sourceMappingURL=index.d.ts.map

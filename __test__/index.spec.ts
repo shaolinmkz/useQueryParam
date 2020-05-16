33a22a48da38271c5a1ea91424b0b79dc993fbe3
shaolinmkz007 <emeka@softcom.ng>
2020-05-16 01:09:28 +0100

    update hook

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,14 +1,14 @@
-import useQueryParam from '../src';
+import { getQueryObject } from '../src';
 
-describe('useQueryParam', () => {
+describe('getQueryObject', () => {
     it('it should return an empty object', () => {
-        const queryParams = useQueryParam();
+        const queryParams = getQueryObject();
         expect(queryParams).toEqual({});
     });
 
     it('it should mock the windows location and return mock params', () => {
         const queries = '?jest=true&typescript=true&hook=true&failed=false';
-        const queryParams = useQueryParam(queries);
+        const queryParams = getQueryObject(queries);
         expect(queryParams).toEqual({
             jest: 'true',
             typescript: 'true',
